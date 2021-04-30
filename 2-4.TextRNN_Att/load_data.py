e71d04175bc3f3081d691aae6c1eaa03a49b03c0
jasoncao11 <tiekuncao@163.com>
2021-04-30 17:28:44 +0800

    update

--- 2-4.TextRNN_Att/load_data.py
+++ 2-4.TextRNN_Att/load_data.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import torch
 import jieba
-from torchtext import data
+from torchtext.legacy import data
 
 device = "cuda" if torch.cuda.is_available() else 'cpu'
 
@@ -12,11 +12,10 @@ def tokenizer(text):
 TEXT = data.Field(sequential=True, tokenize=tokenizer)
 LABEL = data.Field(sequential=False, use_vocab=False)
 
-train, val, test = data.TabularDataset.splits(
+train, val = data.TabularDataset.splits(
         path='../data/', 
         train='train.tsv',
         validation='dev.tsv',
-        test='test.tsv',
         format='tsv',
         skip_header=True,
         fields=[('', None), ('label', LABEL), ('text', TEXT)])
@@ -26,26 +25,8 @@ id2vocab = TEXT.vocab.itos
 #print(TEXT.vocab.stoi)
 #print(TEXT.vocab.itos)
 
-train_iter, val_iter, test_iter = data.BucketIterator.splits(
-        (train, val, test), 
+train_iter, val_iter = data.BucketIterator.splits(
+        (train, val), 
         sort_key=lambda x: len(x.text),
-        batch_sizes=(256, 128, 64), 
-        device=device,
-        sort_within_batch=False,
-        repeat=False)
-
-#for k in train_iter:
-#    text_idx_batch, label_idx_batch = k.text.t_(), k.label
-#    print (text_idx_batch, label_idx_batch)
-#    for x in text_idx_batch:
-#        print(''.join([id2vocab[i.item()] for i in x]))
-#        print('----------------------')
-#    
-#    
-#for k in val_iter:
-#    text_idx_batch, label_idx_batch = k.text.t_(), k.label
-#    print (text_idx_batch, label_idx_batch)
-#    for x in text_idx_batch:
-#        print(''.join([id2vocab[i.item()] for i in x]))
-#        print('----------------------')
-#    print(label_idx_batch)
+        batch_sizes=(256, 128), 
+        device=device)
