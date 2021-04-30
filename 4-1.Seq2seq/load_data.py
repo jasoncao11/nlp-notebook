455653300809923a0406630eabee648127d12d5e
jasoncao11 <tiekuncao@163.com>
2021-04-30 16:52:34 +0800

    update

--- 4-1.Seq2seq/load_data.py
+++ 4-1.Seq2seq/load_data.py
@@ -1,88 +1,39 @@
 # -*- coding: utf-8 -*-
-import csv
 import torch
 import jieba
-import torch.utils.data as tud
+from torchtext.legacy import data
 
-min_freq = 2
-vocab_count = {}
-with open('./data/train.tsv', 'r', encoding='utf8') as rf:
-    r = csv.reader(rf, delimiter='\t')
-    next(r)
-    for row in r:
-        text = f'{row[0]} {row[1]}'
-        for tok in jieba.cut(text):
-            if tok not in vocab_count:
-                vocab_count[tok] = 1
-            else:
-                vocab_count[tok] += 1
+device = "cuda" if torch.cuda.is_available() else 'cpu'
 
-print(len(vocab_count))                
-#vocab_count = sorted(vocab_count.items(), key=lambda x:x[1], reverse=True)[:max_len]
-vocab = [word for word, count in vocab_count.items() if count >= min_freq]
-vocab.extend(['<UNK>', '<PAD>', '<SOS>', '<EOS>'])
-print(len(vocab))
-vocab_size = len(vocab) 
-vocab2idx = {w:i for i, w in enumerate(vocab)}
-#print(vocab2idx['披露'])       
-idx2vocab = {i:w for w, i in vocab2idx.items()}
-#print(idx2vocab[16336])
+def tokenizer(text):
+    token = [tok for tok in jieba.cut(text)]
+    return token
 
-UNK_IDX = vocab2idx['<UNK>']
-PAD_IDX = vocab2idx['<PAD>']
-SOS_IDX = vocab2idx['<SOS>']
-EOS_IDX = vocab2idx['<EOS>']
-PAD_SIZE_C = 350
-PAD_SIZE_S = 35
-BATCH_SIZE = 512
-TRAIN_DATA_PATH = './data/train.tsv'
-DEV_DATA_PATH = './data/dev.tsv'
+TEXT = data.Field(tokenize=tokenizer,
+                  init_token = '<sos>', 
+                  eos_token = '<eos>', 
+                  lower = True, 
+                  batch_first = True)
 
-class SummaryDataset(tud.Dataset):
-    def __init__(self, data_path):
-        super(SummaryDataset, self).__init__()
-        self.content = []
-        self.summary = []
-        with open (data_path, 'r', encoding='utf8') as rf:
-            r = csv.reader(rf, delimiter='\t')
-            next(r)
-            for row in r:
-                self.summary.append(row[0])
-                self.content.append(row[1])
-               
-    def __len__(self):
-        return len(self.summary)
-    
-    def __getitem__(self, idx):
-        content_tokens = [tok for tok in jieba.cut(self.content[idx])]
-        summary_tokens = [tok for tok in jieba.cut(self.summary[idx])]
-        
-        content_idx = [SOS_IDX] + [vocab2idx.get(word, UNK_IDX) for word in content_tokens] + [EOS_IDX]
-        summary_idx = [SOS_IDX] + [vocab2idx.get(word, UNK_IDX) for word in summary_tokens] + [EOS_IDX]
-        
-        content_len = len(content_idx)
-        if content_len < PAD_SIZE_C:
-            content_idx = content_idx + [PAD_IDX] * (PAD_SIZE_C - content_len)
-        else:
-            content_idx = content_idx[:PAD_SIZE_C]
-        
-        summary_len = len(summary_idx)
-        if summary_len < PAD_SIZE_S:
-            summary_idx = summary_idx + [PAD_IDX] * (PAD_SIZE_S - summary_len)
-        else:
-            summary_idx = summary_idx[:PAD_SIZE_S]
-        
-        return torch.tensor(summary_idx), torch.tensor(content_idx)
-        
-traindataset = SummaryDataset(TRAIN_DATA_PATH)
-traindataloader = tud.DataLoader(traindataset, BATCH_SIZE, shuffle=True)
+train, val = data.TabularDataset.splits(
+        path='./data/', 
+        train='train.tsv',
+        validation='dev.tsv',
+        format='tsv',
+        skip_header=True,
+        fields=[('trg', TEXT), ('src', TEXT)])
 
-valdataset = SummaryDataset(DEV_DATA_PATH)
-valdataloader = tud.DataLoader(valdataset, BATCH_SIZE, shuffle=True)
+TEXT.build_vocab(train, min_freq=1)
+id2vocab = TEXT.vocab.itos
+vocab2id = TEXT.vocab.stoi
+PAD_IDX = vocab2id[TEXT.pad_token]
+UNK_IDX = vocab2id[TEXT.unk_token]
+SOS_IDX = vocab2id[TEXT.init_token]
+EOS_IDX = vocab2id[TEXT.eos_token]
 
-#for summary, content in traindataloader:
-#    print(f'summary idx = {summary[0]}')
-#    print(f"summary = {' '.join([idx2vocab[idx.item()] for idx in summary[0]])}")
-#    print(f'content idx = {content[0]}')
-#    print(f"content = {' '.join([idx2vocab[idx.item()] for idx in content[0]])}")
-#    print(f'-----------\n')
+#train_iter 自动shuffle, val_iter 按照sort_key排序
+train_iter, val_iter = data.BucketIterator.splits(
+        (train, val),
+        batch_sizes=(256, 256),
+        sort_key=lambda x: len(x.src),
+        device=device)
--- 4-1.Seq2seq/predict.py
+++ 4-1.Seq2seq/predict.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import jieba
 import torch
-from load_data import vocab_size, UNK_IDX, SOS_IDX, EOS_IDX, vocab2idx, idx2vocab
+from load_data import UNK_IDX, SOS_IDX, EOS_IDX, vocab2id, id2vocab
 from model import Encoder, Decoder, Seq2Seq
 
 device = "cuda" if torch.cuda.is_available() else 'cpu' 
-INPUT_DIM = vocab_size
-OUTPUT_DIM = vocab_size
+INPUT_DIM = len(id2vocab)
+OUTPUT_DIM = len(id2vocab)
 ENC_EMB_DIM = 256
 DEC_EMB_DIM = 256
 HID_DIM = 512
@@ -23,7 +23,7 @@ model.eval()
 
 text = '为了更好地预测网络流量,提出了一种改进型Elman网络模型,并用文化算法对该模型进行了优化,获得了更佳的拟合度和预测性能。'
 tokens = [tok for tok in jieba.cut(text)]
-tokens_idx = [SOS_IDX] + [vocab2idx.get(word, UNK_IDX) for word in tokens] + [EOS_IDX]
+tokens_idx = [SOS_IDX] + [vocab2id.get(word, UNK_IDX) for word in tokens] + [EOS_IDX]
 tokens_idx = torch.tensor(tokens_idx)
 print(tokens_idx)
 res = []
@@ -32,7 +32,7 @@ inputs = torch.tensor([SOS_IDX]).to(device)
 for t in range(1, 35):
     output, hidden, cell = model.decoder(inputs, hidden, cell)
     inputs = output.argmax(1)
-    word = idx2vocab[inputs.item()]
+    word = id2vocab[inputs.item()]
     res.append(word)
     if word == '<EOS>':
         break
