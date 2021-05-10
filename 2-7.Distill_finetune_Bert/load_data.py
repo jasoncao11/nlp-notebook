decfaa4be8c742346cab3e76664d9458d75c8241
jasoncao11 <tiekuncao@163.com>
2021-05-11 01:55:00 +0800

    Update load_data.py

--- 2-7.Distill_finetune_Bert/load_data.py
+++ 2-7.Distill_finetune_Bert/load_data.py
@@ -30,24 +30,23 @@ class BinarySentiDataset(tud.Dataset):
     def __getitem__(self, idx):
         raw_data = self.text[idx]
         label = self.labels[idx]
-        tokens_ids = self.tokenizer.encode(raw_data)
-        
-        seq_len = len(tokens_ids)
-        if seq_len < self.pad_size:
-            mask = [1] * seq_len + [0] * (self.pad_size - seq_len)
-            tokens_ids = tokens_ids + [0] * (self.pad_size - seq_len)
-        else:
-            mask = [1] * self.pad_size
-            tokens_ids = tokens_ids[:self.pad_size]
+        tokens = self.tokenizer.tokenize(raw_data)       
+        seq_len = len(tokens)
+        if seq_len < self.pad_size - 2:
+            tokens = ['[CLS]']+tokens+['[SEP]']+['[PAD]']*(self.pad_size - 2 - seq_len)
+            mask = [1] * (seq_len+2) + [0] * (self.pad_size - seq_len - 2)
             
-        tokens_ids = torch.tensor(tokens_ids)
+        else:
+            tokens = ['[CLS]']+tokens[:self.pad_size - 2]+['[SEP]']
+            mask = [1] * self.pad_size
+            
+        tokens_ids = torch.tensor(self.tokenizer.convert_tokens_to_ids(tokens))
         mask = torch.tensor(mask)
-        label = torch.tensor(label)
-        
+        label = torch.tensor(label)       
         return tokens_ids, mask, label
         
 traindataset = BinarySentiDataset(TRAIN_DATA_PATH, TOKENIZER_PATH, PAD_SIZE)
-traindataloader = tud.DataLoader(traindataset, BATCH_SIZE, shuffle=False)
+traindataloader = tud.DataLoader(traindataset, BATCH_SIZE, shuffle=True)
 
 valdataset = BinarySentiDataset(DEV_DATA_PATH, TOKENIZER_PATH, PAD_SIZE)
 valdataloader = tud.DataLoader(valdataset, BATCH_SIZE, shuffle=False)
